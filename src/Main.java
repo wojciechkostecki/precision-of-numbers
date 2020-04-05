c5c4ac98cd5736d07c98d29803ff2c73b0287b02
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-04-05 21:28:51 +0200

    Printing numbers from 0 to the number given by the user

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        printNumbers();
+    }
+
+    public static void printNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź liczbę całkowitą różną od 0: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            double x = 0;
+            while (x < number) {
+                x += 0.1;
+                System.out.printf("%.1f" + " ", x);
+            }
+        }
+        if (number < 0) {
+            double y = 0;
+            while (y > number) {
+                y -= 0.1;
+                System.out.printf("%.1f" + " ", y);
+            }
+        }
+        if (number == 0) {
+            System.out.println("Wprowadzona liczba nie spełnia warunku.");
+        }
+    }
 }
